cfe05f1997c40c42e0e37ab8f2272870009a556d
Vasil Dakov <vasildakov@gmail.com>
2016-03-09 18:02:35 +0000

    CF

--- src/App/Action/PostcodeAction.php
+++ src/App/Action/PostcodeAction.php
@@ -10,15 +10,15 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class PostcodeAction
 {
-	/**
-	 * @var EntityManager $container
-	 */
-	private $em;
+    /**
+     * @var EntityManager $container
+     */
+    private $em;
 
 
-	/**
-	 * @param ContainerInterface $container
-	 */
+    /**
+     * @param ContainerInterface $container
+     */
     public function __construct(EntityManager $em)
     {
         $this->em   = $em;
@@ -31,26 +31,25 @@ class PostcodeAction
     ) {
     	$postcode = $request->getQueryParams()['postcode'];
 
-    	$validator = new \App\Validator\PostCode;
-    	if(false === $validator->isValid($postcode)) {
-    		return new JsonResponse([
-    			'status' => 'error',
-    			'message' => $validator->getMessages()['error']
-    		]);
-    	}
+        $validator = new \App\Validator\PostCode;
+        if(false === $validator->isValid($postcode)) {
+            return new JsonResponse([
+                'status' => 'error',
+                'message' => $validator->getMessages()['error']
+            ]);
+        }
 
-    	$repository = $this->em->getRepository(Entity\Postcode::class);
-    	$entity = $repository->findOneBy(['postcode' => $postcode]);
+        $repository = $this->em->getRepository(Entity\Postcode::class);
+        $entity = $repository->findOneBy(['postcode' => $postcode]);
 
-    	if($entity instanceof Entity\Postcode) {
-    		return new JsonResponse([
-    			'postcode'  => $entity->getPostcode(),
-    			'latitude'  => (float)$entity->getLatitude(),
-    			'longitude' => (float)$entity->getLongitude(),
-    		]);
-    	}
+        if($entity instanceof Entity\Postcode) {
+            return new JsonResponse([
+                'postcode'  => $entity->getPostcode(),
+                'latitude'  => (float)$entity->getLatitude(),
+                'longitude' => (float)$entity->getLongitude(),
+            ]);
+        }
 
-    	return $next($request, $response);
-        
+        return $next($request, $response);
     }
 }

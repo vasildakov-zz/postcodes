58804e921bebcb4221b80c47f1282258e77e791a
Vasil Dakov <vasildakov@gmail.com>
2016-03-16 11:35:27 +0000

    Updated routes

--- config/autoload/routes.global.php
+++ config/autoload/routes.global.php
@@ -6,10 +6,13 @@ return [
             Zend\Expressive\Router\RouterInterface::class => Zend\Expressive\Router\FastRouteRouter::class,
             App\Action\PingAction::class => App\Action\PingAction::class,
             App\Action\ErrorAction::class => App\Action\ErrorAction::class,
+            App\Action\ValidateAction::class => App\Action\ValidateAction::class,
         ],
         'factories' => [
-            App\Action\HomePageAction::class => App\Action\HomePageFactory::class,
-            App\Action\PostcodeAction::class => App\Action\PostcodeFactory::class,
+            App\Action\HomePageAction::class     => App\Action\HomePageFactory::class,
+            App\Action\LookupAction::class       => App\Action\LookupFactory::class,
+            App\Action\AutocompleteAction::class => App\Action\AutocompleteFactory::class,
+            App\Action\RandomAction::class       => App\Action\RandomFactory::class,
         ],
     ],
 
@@ -27,11 +30,34 @@ return [
             'allowed_methods' => ['GET'],
         ],
         [
-            'name' => 'api.postcode',
-            'path' => '/api/postcode',
+            'name' => 'api.postcodes.lookup',
+            'path' => '/api/postcodes/{postcode}',
             'middleware' => [
-                App\Action\PostcodeAction::class, 
-                App\Action\ErrorAction::class
+                App\Action\LookupAction::class
+            ],
+            'allowed_methods' => ['GET'],
+        ],
+        [
+            'name' => 'api.postcodes.validate',
+            'path' => '/api/postcodes/{postcode}/validate',
+            'middleware' => [
+                App\Action\ValidateAction::class,
+            ],
+            'allowed_methods' => ['GET'],
+        ],
+        [
+            'name' => 'api.postcodes.autocomplete',
+            'path' => '/api/postcodes/{postcode}/autocomplete',
+            'middleware' => [
+                App\Action\AutocompleteAction::class,
+            ],
+            'allowed_methods' => ['GET'],
+        ],
+        [
+            'name' => 'api.postcodes.random',
+            'path' => '/api/random/postcodes',
+            'middleware' => [
+                App\Action\RandomAction::class,
             ],
             'allowed_methods' => ['GET'],
         ],

5f638ef5dec3b12817ebdbf3c9d2b9ab5226009a
Vasil Dakov <vasildakov@gmail.com>
2016-11-08 17:55:50 +0000

    WIP Updated lookup controller

--- src/Application/Action/Lookup.php
+++ src/Application/Action/Lookup.php
@@ -29,16 +29,17 @@ class Lookup implements MiddlewareInterface
      */
     public function __invoke(Request $request, Response $response, callable $next = null)
     {
-        
+
         $postcode = $request->getAttribute('postcode');
-        
+
         $data = $this->repository->findOneByPostcode($postcode);
-        
+
         return new JsonResponse([
             'status' => 200,
             'data' => $data,
         ], 200, [
             "Access-Control-Allow-Origin" => "*",
+            "Access-Control-Allow-Headers" => "*",
             "Access-Control-Allow-Methods" => "GET, POST"
         ]);
     }
